e86f6c4b7df996562714896ffc9fdda7212700b7
Oleksandr Shtyrov <shtyrov89@gmail.com>
2021-03-05 14:56:57 +0200

    import AuthorList added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import AuthorList from './components/Author.js'
 
 class App extends React.Component {
     constructor(props) {
